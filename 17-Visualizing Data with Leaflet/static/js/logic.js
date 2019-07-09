851bfa5a14bd49981910be12dc0804f9c7f952f0
Itunu O <itunuo@Itunus-Air.attlocal.net>
2019-07-08 23:32:05 -0500

    updating geo-mapping hwk

--- 17-Visualizing Data with Leaflet/static/js/logic.js
+++ 17-Visualizing Data with Leaflet/static/js/logic.js
@@ -3,19 +3,13 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  //console.log(data.features)
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 
-
 function createFeatures(earthquakeData) {
-  //console.log(earthquakeData)
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
-    //console.log(feature)
-    //console.log(layer)
-    //console.log(feature.properties.place)
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
@@ -40,7 +34,6 @@ function createFeatures(earthquakeData) {
 };
 
 function createMap(earthquakes) {
-  //console.log(earthquakes)
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
